ff91773bbf0751f2117ae7b57f04f16c0bb7a876
pscschn <pas.schneider@protonmail.ch>
2024-07-02 18:04:28 +0200

    Update configure-tmux.zsh

--- scripts/configure-tmux.zsh
+++ scripts/configure-tmux.zsh
@@ -8,3 +8,5 @@ cp -fr ./tmux/.tmux.conf ~/
 # Reload Tmux configuration and install plugins
 tmux new-session -d -s install-plugins \
     'sleep 1; tmux source-file ~/.tmux.conf; ~/.tmux/plugins/tpm/bin/install_plugins; tmux kill-session -t install-plugins'
+
+tmux source-file ~/.tmux.conf
