7721e8f7fb1dbcf5cdcfdc5fe00c07f4f5d94077
Pascal <pas.schneider@protonmail.ch>
2024-07-13 01:11:01 +0200

    matched plugin structure to template

--- nvim/modules/theming/color-hightlights/init.lua
+++ nvim/modules/theming/color-hightlights/init.lua
@@ -1,11 +1,1 @@
-local index = tonumber((vim.fn.readfile(vim.fn.glob('*index.lua'))[1]))
-
-local module_content = {
-    config = require('config'),
-    install = require('install')
-}
-
-return {
-    module = module_content,
-    index = index
-}
+return require('nvim-385.plugin'):new(require("nvim-385.utils").pwd())
