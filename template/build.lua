afffd58566d9b410a05975fd932ba7d8c14cdbb6
Pascal <pas.schneider@protonmail.ch>
2024-07-13 00:53:53 +0200

    Build configs and keymaps

---
+++ template/build.lua
@@ -0,0 +1,20 @@
+local utils = require('utils')
+local plugins = require('load_modules')
+local module = require('module')
+
+local generate_content = function(file)
+  local contents = "return { \n"
+
+  for _, value in pairs(plugins) do
+      contents = contents .. "dofile('" .. value.module .. "/" .. file .. "')," .. "\n"
+    end
+
+  contents = contents .. "}"
+  return contents
+end
+
+local path = utils.getAbsolutePath(module.pwd(), "../.build")
+utils.mkdir_p(path)
+utils.writeToFile(path, 'plugins.lua', generate_content( 'install.lua'))
+utils.writeToFile(path, 'configs.lua', generate_content( 'config.lua'))
+utils.writeToFile(path, 'keymaps.lua', generate_content( 'keymaps.lua'))
