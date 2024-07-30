004226e7ebf92f9af8f39dbb0ba20e31be9c071c
pscschn <167255482+pscschn@users.noreply.github.com>
2024-07-30 08:10:00 +0200

    Made scripts more distro compatible

--- scripts/install-all.zsh
+++
@@ -1,9 +0,0 @@
-#! /bin/zsh
-#
-# Runs the scripts inside ./install
-
-: "${ROOT_DIR:=$(pwd)}"
-
-source $ROOT_DIR/scripts/install/nvim.zsh
-source $ROOT_DIR/scripts/install/ripgrep.zsh
-source $ROOT_DIR/scripts/install/just.zsh
