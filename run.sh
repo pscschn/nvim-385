3e7ec05132562dbdba3b5c5a59e6c3685be1e8d2
pscschn <pas.schneider@protonmail.ch>
2025-06-07 21:35:58 +0000

    run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# Build and run
+
+set -ex
+
+podman build \
+  -f https://raw.githubusercontent.com/pscschn/nvim-385/refs/heads/master/Dockerfile \
+  -t nvim-385 . && \
+podman run \
+  --replace -it \
+  --name nvim-385 \
+  -v ./:/mnt/ \
+  localhost/nvim-385 bash -c nvim
