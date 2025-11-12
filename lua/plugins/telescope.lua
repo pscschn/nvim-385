0ab19123efd414c6ec3d4564c873e24d175346d2
Pascal Schneider <167255482+pscschn@users.noreply.github.com>
2025-11-12 14:36:54 +0100

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,14 @@ return {
     "nvim-telescope/telescope-symbols.nvim",
   },
   opts = {
+    file_ignore_patterns = {
+      "^.git/",
+      "%.git/",
+      "node_modules/",
+      "%.jpg$", "%.png$", "%.jpeg$", "%.gif$",
+      "%.o$", "%.obj$", "%.dll$", "%.exe$",
+      "%.so$",
+    },
     pickers = {
       find_files = {
         hidden = true,
